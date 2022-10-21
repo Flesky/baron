e7769fb9961a609f9f01609774defd38796a4bb4
Baron <qwertyuiop.br18@gmail.com>
2022-10-21 23:58:18 +0800

    Initial commit

---
+++ src/components/home/app-icon.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-col items-center gap-2 p-2">
+    <img src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" alt="Vuetify Logo" class="aspect-square w-12 rounded-2xl bg-white p-2">
+    <div class="text-center text-sm">
+      Vuetify
+    </div>
+  </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import AppIcon from '@/components/home/app-icon.vue'
+import 'swiper/css'
+const formatted = useDateFormat(useNow(), 'h:mm A')
+</script>
+
+<template>
+  <div id="home" class="flex min-h-full flex-col ">
+    <div class="flex h-8 shrink-0 items-center justify-center">
+      <div>
+        {{ formatted }}
+      </div>
+    </div>
+    <div class="flex grow">
+      <Swiper :space-between="32">
+        <SwiperSlide class="grid grid-cols-4 grid-rows-6  items-center gap-x-3 p-4">
+          <AppIcon />
+          <AppIcon />
+          <AppIcon />
+          <AppIcon />
+          <AppIcon />
+        </SwiperSlide>
+        <SwiperSlide class="flex items-center justify-center p-4">
+          Work in progress ;)
+        </SwiperSlide>
+      </Swiper>
+    </div>
+    <div class="h-12 shrink-0" />
+  </div>
+</template>
+
+<style>
+#home {
+  background: url("@/assets/images/wallpapers/1.jpg");
+  background-size: cover;
+}
+</style>
